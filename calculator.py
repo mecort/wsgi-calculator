6f2dcebf1bb1aeded78550f8d6301d0d42831b16
Michael Cort <mcort@uw.edu>
2020-10-18 14:37:50 -0700

    Completed implementation, all tests pass and works as expected.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import traceback
+
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -45,13 +47,60 @@ To submit your homework:
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+    result = int(args[0]) + int(args[1])
 
-    return sum
+    return str(result)
 
-# TODO: Add functions for handling more arithmetic operations.
+
+def subtract(*args):
+    """ Returns a STRING with the sum of the arguments """
+
+    result = int(args[0]) - int(args[1])
+
+    return str(result)
+
+
+def multiply(*args):
+    """ Returns a STRING with the sum of the arguments """
+
+    result = int(args[0]) * int(args[1])
+
+    return str(result)
+
+
+def divide(*args):
+    """ Returns a STRING with the sum of the arguments """
+
+    result = int(args[0]) / int(args[1])
+
+    return str(result)
+
+
+def welcome():
+    """provides a welcome page to users at the root directory"""
+    page = """<html>
+<head>
+<title>Michael's Calculator</title>
+</head>
+<body>
+<h2>Here's how to use this page:</h2>
+<p>You will need to provide three inputs:
+the operator and the two values you would like to use in the equation.</p>
+<p>If you wanted to add 5 and 6 together, you would input:
+".../add/5/6", and the page will return "11"</p>
+<p>This website will accept the following operators:</p>
+<ul>
+  <li>add</li>
+  <li>subtract</li>
+  <li>multiply</li>
+  <li>divide</li>
+</ul>
+<p>The order of the operators is important in methods like subtracting and dividing,
+so input them in order from left to right. </p>
+</body>
+</html>"""
+    return page
+
 
 def resolve_path(path):
     """
@@ -59,26 +108,51 @@ def resolve_path(path):
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    funcs = {
+        '': welcome,
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide,
+    }
+
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
